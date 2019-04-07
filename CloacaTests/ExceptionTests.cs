62089a07f58292d3ac8c859b7b9a745fe9f69008
rocko <adam.preble@gmail.com>
2019-04-07 11:20:37 -0500

    Added more exception handling test stubs.
    Made dedicated exception alias tests. Ultimately, we'll need to subclass an
    Exception with some field to muck with and prove it worked.

--- CloacaTests/ExceptionTests.cs
+++ CloacaTests/ExceptionTests.cs
@@ -20,15 +20,30 @@ namespace CloacaTests
 
         [Test]
         [Ignore("Exception handling not implemented")]
+        public void TryExceptBlank()
+        {
+            var interpreter = runProgram(
+                "a = 0\n" +
+                "try:\n" +
+                "  raise Exception('Hello, World!')\n" +
+                "except\n" +
+                "  a = a + 10\n", new Dictionary<string, object>(), 1);
+            var variables = new VariableMultimap(interpreter);
+            var a = (BigInteger)variables.Get("a");
+            Assert.That(a, Is.EqualTo(new BigInteger(10)));
+        }
+
+        [Test]
+        [Ignore("Exception handling not implemented")]
         public void TryExceptFinally()
         {
             var interpreter = runProgram(
                 "a = 0\n" +
                 "try:\n" +
                 "  raise Exception('Hello, World!')\n" +
-                "except Exception as e:\n" + 
+                "except Exception\n" +
                 "  a = a + 10\n" +
-                "finally:\n" + 
+                "finally:\n" +
                 "  a = a + 1\n", new Dictionary<string, object>(), 1);
             var variables = new VariableMultimap(interpreter);
             var a = (BigInteger)variables.Get("a");
@@ -52,6 +67,39 @@ namespace CloacaTests
             var variables = new VariableMultimap(interpreter);
             var a = (BigInteger)variables.Get("a");
             Assert.That(a, Is.EqualTo(new BigInteger(1101)));
+        }
+
+        [Test]
+        [Ignore("Exception handling not implemented")]
+        public void TryExceptAliasBasic()
+        {
+            var interpreter = runProgram(
+                "a = 0\n" +
+                "try:\n" +
+                "  raise Exception('Hello, World!')\n" +
+                "except Exception as e\n" +
+                "  a = a + 10\n", new Dictionary<string, object>(), 1);
+            var variables = new VariableMultimap(interpreter);
+            var a = (BigInteger)variables.Get("a");
+            Assert.That(a, Is.EqualTo(new BigInteger(11)));
+        }
+
+        [Test]
+        [Ignore("Exception handling not implemented; subclassing not implemented yet.")]
+        public void TryExceptAliasUseValue()
+        {
+            var interpreter = runProgram(
+                "class MeowException(Exception):\n" +
+                "  def __init__(self, number):\n" +
+                "    self.number = number\n" +
+                "a = 0\n" +
+                "try:\n" +
+                "  raise MeowException(1)\n" +
+                "except MeowException as e\n" +
+                "  a = e\n", new Dictionary<string, object>(), 1);
+            var variables = new VariableMultimap(interpreter);
+            var a = (BigInteger)variables.Get("a");
+            Assert.That(a, Is.EqualTo(new BigInteger(1)));
         }
     }
 }
