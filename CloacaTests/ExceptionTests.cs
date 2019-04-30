aaa3679e3dc21792e5d857c51598904f0253f6c5
rocko <adam.preble@gmail.com>
2019-04-30 00:36:46 -0500

    Cleanup and extensions to exception handling tests

--- CloacaTests/ExceptionTests.cs
+++ CloacaTests/ExceptionTests.cs
@@ -42,7 +42,7 @@ namespace CloacaTests
                 "a = 0\n" +
                 "try:\n" +
                 "  raise Exception('Hello, World!')\n" +
-                "except Exception\n" +
+                "except Exception:\n" +
                 "  a = a + 10\n" +
                 "finally:\n" +
                 "  a = a + 1\n", new Dictionary<string, object>(), 1);
@@ -78,7 +78,7 @@ namespace CloacaTests
                 "a = 0\n" +
                 "try:\n" +
                 "  raise Exception('Hello, World!')\n" +
-                "except Exception as e\n" +
+                "except Exception as e:\n" +
                 "  a = a + 10\n", new Dictionary<string, object>(), 1);
             var variables = new VariableMultimap(interpreter);
             var a = (BigInteger)variables.Get("a");
@@ -96,7 +96,27 @@ namespace CloacaTests
                 "a = 0\n" +
                 "try:\n" +
                 "  raise MeowException(1)\n" +
-                "except MeowException as e\n" +
+                "except MeowException as e:\n" +
+                "  a = e\n", new Dictionary<string, object>(), 1);
+            var variables = new VariableMultimap(interpreter);
+            var a = (BigInteger)variables.Get("a");
+            Assert.That(a, Is.EqualTo(new BigInteger(1)));
+        }
+
+        [Test]
+        [Ignore("Exception handling not implemented; subclassing not implemented yet.")]
+        public void TryExceptTwoExceptions()
+        {
+            var interpreter = runProgram(
+                "class MeowException(Exception):\n" +
+                "  def __init__(self, number):\n" +
+                "    self.number = number\n" +
+                "a = 0\n" +
+                "try:\n" +
+                "  raise MeowException(1)\n" +
+                "except Exception as ignored\n" +
+                "  a = ignored\n" +
+                "except MeowException as e:\n" +
                 "  a = e\n", new Dictionary<string, object>(), 1);
             var variables = new VariableMultimap(interpreter);
             var a = (BigInteger)variables.Get("a");
