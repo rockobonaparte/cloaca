0d32a3bb0747037f0b115b7097d8f76de8ec7014
rocko <adam.preble@gmail.com>
2019-08-05 01:59:26 -0500

    Started to sketch PyInteger
    Some scaffolding is here that would be used for creating the other basic
    object types. None of this is connected to anything yet and is prone to
    change.

---
+++ LanguageImplementation/DataTypes/PyInteger.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace LanguageImplementation.DataTypes
+{
+    public class PyIntegerClass : PyTypeObject
+    {
+        public PyIntegerClass(CodeObject __init__, IInterpreter interpreter, FrameContext context,
+            PyClass[] bases) :
+            base("int", __init__, interpreter, context)
+        {
+            __bases__ = bases;
+
+            var classMembers = GetType().GetMethods().Where(m => m.GetCustomAttributes(typeof(ClassMember), false).Length > 0).ToArray();
+
+            foreach (var classMember in classMembers)
+            {
+                this.__dict__[classMember.Name] = new WrappedCodeObject(classMember.Name, classMember);
+            }
+
+        }
+
+        [ClassMember]
+        public static PyObject __add__(PyObject self, PyObject other)
+        {
+            var a = self as PyInteger;
+            var b = other as PyInteger;
+            if (a == null)
+            {
+                throw new Exception("Tried to use a non-PyInteger for addition");
+            }
+            if (b == null)
+            {
+                throw new Exception("Tried to add a PyInteger to a non-PyInteger");
+            }
+
+            var newPyInteger = new PyInteger(a.number + b.number);
+            return newPyInteger;
+        }
+    }
+
+    public class PyInteger : PyObject
+    {
+        public BigInteger number;
+        public PyInteger(BigInteger num)
+        {
+            number = num;
+        }
+
+        public PyInteger()
+        {
+            number = 0;
+        }
+    }
+
+    public class ClassMember : System.Attribute
+    {
+
+    }
+}
