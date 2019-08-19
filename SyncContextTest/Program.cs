d2bd9447d66c419d769776aa608af7d7f663854e
rocko <adam.preble@gmail.com>
2019-08-19 02:58:49 -0500

    Starting to doodle ways of using Tasks instead of IEnumerator coroutines
    It's in a side project that will mimick the kind of flow I'm doing in Cloaca.
    Included is a bunch of stuff ripped online to show how to pause some code,
    although I don't think it's being done cooperative.

---
+++ SyncContextTest/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// These represent frames managed by the interpreter. Frames are the complete context of an
+/// executing script. This would include the complete stack trace and variable conditions. 
+/// These frames are stopped and started at will.
+/// 
+/// We aren't actually implementing that feature here for this an are just using direct C#
+/// calls, but the naming is place to convey the intent.
+/// </summary>
+class InterpreterFrame
+{
+    Callable script;
+    public InterpreterFrame(Callable script)
+    {
+        this.script = script;
+    }
+
+    public void Run()
+    {
+        script.Run();
+    }
+}
+
+interface Callable
+{
+    void Run();
+}
+
+class MockInterpreter
+{
+    public List<InterpreterFrame> frames;
+
+    public void AddScript(Callable script)
+    {
+        frames.Add(new InterpreterFrame(script));
+    }
+
+    public void Tick()
+    {
+        foreach(var frame in frames)
+        {
+            frame.Run();
+        }
+    }
+}
+
+class DialogRequest
+{
+    public DialogRequest()
+    {
+        // Does nothing yet. We'll need to plumb a callback and blocking mechanism or whatever before
+        // this goes live.
+    }
+
+    public void SignalDone()
+    {
+
+    }
+}
+
+/// <summary>
+/// Representing a subsystem that's taking care of GUI dialogs. This mocks printing some text to the
+/// screen and block control until it's acknowledged with a button press. However, it can't literally
+/// block the engine or else the control input would never get acknowledged. So this has to call back
+/// to the requestor once acknowledged. The trick is we want to make these requests using very simple,
+/// procedural calls within a scripting interpreter. So we want those calls to look very natural
+/// (half-sync/half-async kind of thing).
+/// </summary>
+class DialogSubsystem
+{
+    private Queue<DialogRequest> requests;
+    private DialogRequest activeRequest;
+
+    public DialogSubsystem()
+    {
+        requests = new Queue<DialogRequest>();
+    }
+
+    public void Tick()
+    {
+        // An active request will basic run for one "frame." In reality, these prompts would be up for
+        // many more frames than that, but having this is enough to show the temporal situation.
+        if(activeRequest != null)
+        {
+            activeRequest.SignalDone();
+            activeRequest = null;
+        }
+        if (requests.Count > 0)
+        {
+            activeRequest = requests.Dequeue();
+        }
+    }
+
+    public void Say(DialogRequest request)
+    {
+        requests.Enqueue(request);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var dialogEngine = new DialogSubsystem();
+        var interpreter = new MockInterpreter();
+
+        // Mimicking Unity game engine loop. Each iteration is a frame.
+        for(int frame = 0; frame < 100; ++frame)
+        {
+            dialogEngine.Tick();
+            interpreter.Tick();
+        }
+    }
+}
---
+++ SyncContextTest/plan.md
@@ -0,0 +1,44 @@
+﻿# What We're Trying To Do
+
+Currently, we are using IEnumerator coroutines. This has a few problems:
+1. Pollution of function signatures to all return IEnumerators
+2. Copy-paste of a common iteration loop over these calls to get them to complete, with intermediate results just getting percolated upwards.
+
+Let's try something--anything--else. First, Tasks in general.
+
+Let's consider a scenario similar to how we're using this interpreter. The interpreter is embedded into Unity as its own subsystem running on the main
+thread. This subsystem is tracking how much time it's blowing in the current frame and will let off if it either is about to exceed that or all of its
+tasks are blocked. The tasks that are blocked are usually blocked on getting results from outside of the interpreter's subsystem.
+
+Let's create two tasks:
+1. A long-running task that is just cooperative yielding. Note that the cooperative yielding may be coming from the interpreter itself, but we'll
+   just mock that with explicit mock callouts to pause.
+2. A second task waiting for the result of something that will run outside of the scope of the interpreter's scheduler. A lot of such subsystem calls
+   might be satisfied immediately, but this one should wait. It should theoretically wait at least one rendering frame.
+
+There is an implicit third task: The outer context that will represent an external subsystem that supplies results.
+
+For maximum replication, the yielding should happen a few layers from the top-level call so we can see what it looks like to propagate blockages.
+
+All of this should run cooperatively on one thread.
+
+# Rough Sketch
+
+## Long Running Task
+```
+Outer:
+  Increase a counter
+  yield
+  Inner (this is called as a separate function so we can see what it's like to chain into independent, yielding code)
+    Loop 5 Times:
+       Increase a counter
+       cooperative pause
+    return some value
+```
+
+## Requesting Task
+```
+Loop 5 Times:
+   Request something from external subsystems; the external subsystem should block for one iteration before supplying it
+  (this task should then pause)
+```
