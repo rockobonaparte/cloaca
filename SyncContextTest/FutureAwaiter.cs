36ab7aa185a647eb83e361903daa52921a0791c2
rocko <adam.preble@gmail.com>
2019-09-02 02:15:14 -0500

    Collapsed the FutureVoidAwaiter into a subclass of FutureAwaiter with sentinel

--- SyncContextTest/FutureAwaiter.cs
+++ SyncContextTest/FutureAwaiter.cs
@@ -69,57 +69,48 @@ public class FutureAwaiter<T> : System.Runtime.CompilerServices.INotifyCompletio
 }
 
 
-// A void version. We can't use void as a parameter type.
-public class FutureVoidAwaiter : INotifyCompletion, ISubscheduledContinuation
+/// <summary>
+/// Helper class to give the FutureAwaiter a void type. We can't do that by default since we can't
+/// set void as a result type. We don't want to rely on null since we want a null result in the FutureAwaiter
+/// to indicate actual null--or NoneType per the interpreter. Hence, we declare a custom object type. It is
+/// implied that the interpreter will keep an eye out for this sentinel from FutureAwaiters and not push
+/// anything on the stack if it sees it.
+/// </summary>
+public class VoidSentinel
 {
-    private bool finished;
-    private Action continuation;
-    MockInterpreter interpreter;
+    private VoidSentinel()
+    {
 
-    public bool IsCompleted
+    }
+
+    private static VoidSentinel __instance;
+
+    public static VoidSentinel Instance
     {
         get
         {
-            return finished;
+            if (__instance == null)
+            {
+                __instance = new VoidSentinel();
+            }
+            return __instance;
         }
     }
+}
 
-    public FutureVoidAwaiter(MockInterpreter interpreterToSubschedule)
+// A void version because we can't use void as a parameter type.
+public class FutureVoidAwaiter : FutureAwaiter<VoidSentinel>
+{
+    public FutureVoidAwaiter(MockInterpreter interpreterToSubschedule) : base(interpreterToSubschedule)
     {
-        finished = false;
-        interpreter = interpreterToSubschedule;
     }
 
+    /// <summary>
+    /// Sets the result to the void sentinel. This is just a convenience helper so consumers don't need to
+    /// actually know about the void sentinel.
+    /// </summary>
     public void SignalDone()
     {
-        finished = true;
-        interpreter.NotifyUnblocked(this);
-    }
-
-    public void Continue()
-    {
-        continuation?.Invoke();
-    }
-
-    public void OnCompleted(Action continuation)
-    {
-        if (finished)
-        {
-            this.continuation();
-        }
-        else
-        {
-            this.continuation = continuation;
-        }
-    }
-
-    public FutureVoidAwaiter GetAwaiter()
-    {
-        return this;
-    }
-
-    public void GetResult()
-    {
-
+        SetResult(VoidSentinel.Instance);
     }
 }
