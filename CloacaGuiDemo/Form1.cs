ec1cf5c7a39c9108f758ef265e57a0670b380f99
rocko <adam.preble@gmail.com>
2019-11-26 23:50:07 -0600

    Filled a dummy program into the demo GUI

--- CloacaGuiDemo/Form1.cs
+++ CloacaGuiDemo/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -7,6 +8,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using Antlr4.Runtime;
+
+using CloacaInterpreter;
+using Language;
+using LanguageImplementation;
 
 namespace CloacaGuiDemo
 {
@@ -19,7 +26,56 @@ namespace CloacaGuiDemo
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            string program = "a = 1";
 
+            var inputStream = new AntlrInputStream(program);
+            var lexer = new CloacaLexer(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+            var errorListener = new ParseErrorListener();
+            var parser = new CloacaParser(commonTokenStream);
+            parser.AddErrorListener(errorListener);
+            if (errorListener.Errors.Count > 0)
+            {
+                Console.WriteLine("There were errors trying to compile the script. We cannot run it.");
+                return;
+            }
+
+            var antlrVisitorContext = parser.program();
+
+            var variablesIn = new Dictionary<string, object>();
+            var visitor = new CloacaBytecodeVisitor(variablesIn);
+            visitor.Visit(antlrVisitorContext);
+
+            CodeObject compiledProgram = visitor.RootProgram.Build();
+
+            var scheduler = new Scheduler();
+            var interpreter = new Interpreter(scheduler);
+            scheduler.SetInterpreter(interpreter);
+
+            var context = scheduler.Schedule(compiledProgram);
+            foreach (string varName in variablesIn.Keys)
+            {
+                context.SetVariable(varName, variablesIn[varName]);
+            }
+
+            while(!scheduler.Done)
+            {
+                try
+                {
+                    scheduler.Tick().Wait();
+                }
+                catch (AggregateException wrappedEscapedException)
+                {
+                    // Given the nature of exception handling, we should normally only have one of these!
+                    ExceptionDispatchInfo.Capture(wrappedEscapedException.InnerExceptions[0]).Throw();
+                }
+            }
+
+            var variables = context.DumpVariables();
+            foreach(var var_pair in variables)
+            {
+                richTextBox1.Text += var_pair.Key + " = " + var_pair.Value.ToString() + "\n";
+            }
         }
 
         private void tabPage1_Click(object sender, EventArgs e)
