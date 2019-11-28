a93a053492ae3dc6f9b92f425aec2121c8bdc81e
rocko <adam.preble@gmail.com>
2019-11-28 01:30:26 -0600

    Prototyping a parse error listener to detect when we need more input
    If it's expecting INDENT then we'll assume for now that means the expression
    is incomplete!

--- CloacaGuiDemo/Form1.cs
+++ CloacaGuiDemo/Form1.cs
@@ -10,6 +10,10 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using Antlr4.Runtime;
+using Antlr4.Runtime.Atn;
+using Antlr4.Runtime.Dfa;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Sharpen;
 
 using CloacaInterpreter;
 using Language;
@@ -17,6 +21,56 @@ using LanguageImplementation;
 
 namespace CloacaGuiDemo
 {
+    public class ReplParseErrorListener : IParserErrorListener
+    {
+        public List<string> Errors;
+        public bool needsMoreText;
+
+        public ReplParseErrorListener()
+        {
+            Errors = new List<string>();
+            needsMoreText = false;
+        }
+
+        public void ReportAmbiguity([NotNull] Parser recognizer, [NotNull] DFA dfa, int startIndex, int stopIndex, bool exact, [Nullable] BitSet ambigAlts, [NotNull] ATNConfigSet configs)
+        {
+            // Ignore
+        }
+
+        public void ReportAttemptingFullContext([NotNull] Parser recognizer, [NotNull] DFA dfa, int startIndex, int stopIndex, [Nullable] BitSet conflictingAlts, [NotNull] SimulatorState conflictState)
+        {
+            // Ignore
+        }
+
+        public void ReportContextSensitivity([NotNull] Parser recognizer, [NotNull] DFA dfa, int startIndex, int stopIndex, int prediction, [NotNull] SimulatorState acceptState)
+        {
+            // Ignore
+        }
+
+        public void SyntaxError([NotNull] IRecognizer recognizer, [Nullable] IToken offendingSymbol, int line, int charPositionInLine, [NotNull] string msg, [Nullable] RecognitionException e)
+        {
+            var expected_tokens = e.GetExpectedTokens();
+            if(expected_tokens.Count > 0 && expected_tokens.Contains(CloacaParser.INDENT))
+            {
+                needsMoreText = true;
+            }
+            Errors.Add("line " + line + ":" + charPositionInLine + " " + msg);
+        }
+
+        public string Report()
+        {
+            var report = "";
+
+            foreach (var line in Errors)
+            {
+                report += line + "\n";
+            }
+
+            return report;
+        }
+    }
+
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -26,14 +80,14 @@ namespace CloacaGuiDemo
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //string program = "a = 1\na = a + 1";
+            //string program = "a = 1\na = a + 1\n";
             // TODO: This shouldn't fully parse. Or rather, we should know that this isn't a complete program and we have more work to do.
             string program = "if True:\n";
 
             var inputStream = new AntlrInputStream(program);
             var lexer = new CloacaLexer(inputStream);
             CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
-            var errorListener = new ParseErrorListener();
+            var errorListener = new ReplParseErrorListener();
             var parser = new CloacaParser(commonTokenStream);
             parser.AddErrorListener(errorListener);
             if (errorListener.Errors.Count > 0)
@@ -43,6 +97,10 @@ namespace CloacaGuiDemo
             }
 
             var antlrVisitorContext = parser.program();
+            if(errorListener.needsMoreText)
+            {
+                richTextBox1.Text += "Needs more text\n";
+            }
 
             var variablesIn = new Dictionary<string, object>();
             var visitor = new CloacaBytecodeVisitor(variablesIn);
