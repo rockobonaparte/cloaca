ae98b44bace44f5951d6424bb2c2ca88efb4302d
rocko <adam.preble@gmail.com>
2019-12-06 02:22:31 -0600

    Initial, tested implementation of an injector to use for wrapped code objects
    This should take over for the pseudo-injection happening with them already,
    but it hasn't been installed.

---
+++ CloacaTests/InjectionTests.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using LanguageImplementation;
+using CloacaInterpreter;
+using System.Reflection;
+using System;
+
+namespace CloacaTests
+{
+    [TestFixture]
+    public class InjectionTests
+    {
+        public void NoArgsFunction()
+        {
+
+        }
+
+        public void WantsInterpreterFunction(IInterpreter interpreter)
+        {
+
+        }
+
+        public void WantsBothFunction(IInterpreter interpreter, FrameContext context)
+        {
+
+        }
+
+        public void WantsBothAndArgFunction(IInterpreter interpreter, FrameContext context, int i)
+        {
+
+        }
+
+        public void WantsBothAndArgAndParamFunction(IInterpreter interpreter, FrameContext context, int i, params string[] extras)
+        {
+
+        }
+
+        public void WantsOnlyParamFunction(params string[] extras)
+        {
+
+        }
+
+        Interpreter interpreter;
+        FrameContext context;
+        Injector injector;
+
+        [SetUp]
+        public void Setup()
+        {
+            var scheduler = new Scheduler();
+            interpreter = new Interpreter(scheduler);
+            context = new FrameContext(null);
+            injector = new Injector();
+            injector.Interpreter = interpreter;
+            injector.Context = context;
+        }
+
+        [Test]
+        public void NoArgs()
+        {
+            var out_args = injector.Inject(typeof(InjectionTests).GetMethod(nameof(NoArgsFunction)), new object[0]);
+            Assert.That(out_args.Length, Is.Zero);
+        }
+
+        [Test]
+        public void WantsInterpreter()
+        {
+            var out_args = injector.Inject(typeof(InjectionTests).GetMethod(nameof(WantsInterpreterFunction)), new object[0]);
+            Assert.That(out_args.Length, Is.EqualTo(1));
+            Assert.That(out_args[0], Is.EqualTo(interpreter));
+        }
+
+        [Test]
+        public void WantsOnlyParamSpecified()
+        {
+            var out_args = injector.Inject(typeof(InjectionTests).GetMethod(nameof(WantsOnlyParamFunction)), new object[1] { "Yay!" });
+            Assert.That(out_args.Length, Is.EqualTo(1));
+            var params_array = (string[])out_args[0];
+            Assert.That(params_array[0], Is.EqualTo("Yay!"));
+        }
+
+        [Test]
+        public void WantsOnlyParamUnspecified()
+        {
+            var out_args = injector.Inject(typeof(InjectionTests).GetMethod(nameof(WantsOnlyParamFunction)), new object[0]);
+            Assert.That(out_args.Length, Is.EqualTo(1));
+            Assert.That(out_args[0], Is.Null);
+        }
+
+        [Test]
+        public void WantsBoth()
+        {
+            var out_args = injector.Inject(typeof(InjectionTests).GetMethod(nameof(WantsBothFunction)), new object[0]);
+            Assert.That(out_args.Length, Is.EqualTo(2));
+            Assert.That(out_args[0], Is.EqualTo(interpreter));
+            Assert.That(out_args[1], Is.EqualTo(context));
+        }
+
+        [Test]
+        public void WantsBothAndArg()
+        {
+            var out_args = injector.Inject(typeof(InjectionTests).GetMethod(nameof(WantsBothAndArgFunction)), new object[1] { 1 });
+            Assert.That(out_args.Length, Is.EqualTo(3));
+            Assert.That(out_args[0], Is.EqualTo(interpreter));
+            Assert.That(out_args[1], Is.EqualTo(context));
+            Assert.That(out_args[2], Is.EqualTo(1));
+        }
+
+        [Test]
+        public void WantsBothAndArgAndParamSpecified()
+        {
+            var out_args = injector.Inject(typeof(InjectionTests).GetMethod(nameof(WantsBothAndArgAndParamFunction)), new object[2] { 1, "Yay!"});
+            Assert.That(out_args.Length, Is.EqualTo(4));
+            Assert.That(out_args[0], Is.EqualTo(interpreter));
+            Assert.That(out_args[1], Is.EqualTo(context));
+            Assert.That(out_args[2], Is.EqualTo(1));
+            var params_array = (string[]) out_args[3];
+            Assert.That(params_array[0], Is.EqualTo("Yay!"));
+        }
+
+        [Test]
+        public void WantsBothAndArgAndParamUnspecified()
+        {
+            var out_args = injector.Inject(typeof(InjectionTests).GetMethod(nameof(WantsBothAndArgAndParamFunction)), new object[1] { 1 });
+            Assert.That(out_args.Length, Is.EqualTo(4));
+            Assert.That(out_args[0], Is.EqualTo(interpreter));
+            Assert.That(out_args[1], Is.EqualTo(context));
+            Assert.That(out_args[2], Is.EqualTo(1));
+            Assert.That(out_args[3], Is.Null);
+        }
+
+    }
+}
---
+++ LanguageImplementation/Injector.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageImplementation
+{
+    /// <summary>
+    /// Helps WrappedCodeObjects inject the interpreter and frame context into wrapped calls that need them. Give it the current interpreter and frame
+    /// context. Then when calls are being made, it'll take the actual arguments it has 
+    /// </summary>
+    public class Injector
+    {
+        public IInterpreter Interpreter;
+        public FrameContext Context;
+
+        public void Prepare(IInterpreter interpreter, FrameContext context)
+        {
+            Interpreter = interpreter;
+            Context = context;
+        }
+
+        public object[] Inject(MethodInfo methodInfo, object[] args)
+        {
+            var methodParams = methodInfo.GetParameters();
+
+            // Let's assume usually we don't need to actually do anything. In such cases, return args since it's already okay.
+            bool mustInject = false;
+            
+            // If there's a params field then we have to cram an array into there.
+            bool hasParamsField = methodParams.Length >= 1 && methodParams[methodParams.Length - 1].IsDefined(typeof(ParamArrayAttribute), false);
+            mustInject |= hasParamsField;
+            if (!mustInject)
+            {
+                // Okay, let's see if we need to inject an interpreter or frame context.
+                foreach(var paramInfo in methodParams)
+                {
+                    if(paramInfo.ParameterType == typeof(IInterpreter) ||
+                       paramInfo.ParameterType == typeof(FrameContext))
+                    {
+                        mustInject = true;
+                        break;
+                    }
+                }
+            }
+
+            if(!mustInject)
+            {
+                return args;
+            }
+
+            // If we're here, then it's injection time!
+
+            // Transform all arguments except for any in the params field.
+            object[] outParams = new object[methodParams.Length];
+            int in_param_i = 0;     // Keep an eye on this for later to determine if we have stuff for a params field!
+            for(int out_param_i = 0; out_param_i < (hasParamsField ? methodParams.Length - 1 : methodParams.Length); ++out_param_i)
+            {
+                var paramInfo = methodParams[out_param_i];
+                if(paramInfo.ParameterType == typeof(IInterpreter))
+                {
+                    outParams[out_param_i] = Interpreter;
+                }
+                else if(paramInfo.ParameterType == typeof(FrameContext))
+                {
+                    outParams[out_param_i] = Context;
+                }
+                else
+                {
+                    outParams[out_param_i] = args[in_param_i];
+                    ++in_param_i;
+                }
+            }
+
+            // If there's a params field and we don't have enough stuff to fill it, then we need to
+            // give it a null or else we'll run into a TargetParameterCountException
+            // If we *can* fill it in, we need to convert to the params array type.
+            //
+            if (hasParamsField)
+            {
+                if (in_param_i >= args.Length)
+                {
+                    outParams[outParams.Length - 1] = null;
+                }
+                else
+                {
+                    var elementType = methodParams[methodParams.Length - 1].ParameterType.GetElementType();
+                    var paramsArray = Array.CreateInstance(elementType, args.Length - in_param_i);
+                    for (int i = 0; i < paramsArray.Length; ++i)
+                    {
+                        paramsArray.SetValue(args[in_param_i + i], i);
+                    }
+
+                    outParams[outParams.Length - 1] = paramsArray;
+                }
+            }
+
+            return outParams;
+        }
+    }
+}
