ebd32594c1c564fc7a2361e70faa5ab1052dd16a
rocko <adam.preble@gmail.com>
2019-12-13 01:35:57 -0600

    Sketching out some GUI controls to manipulate in REPL demo

--- CloacaGuiDemo/Form1.Designer.cs
+++ CloacaGuiDemo/Form1.Designer.cs
@@ -35,10 +35,32 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.playerXLabel = new System.Windows.Forms.Label();
+            this.playerYLabel = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.blip0 = new System.Windows.Forms.Label();
+            this.blip1 = new System.Windows.Forms.Label();
+            this.blip2 = new System.Windows.Forms.Label();
+            this.blip3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dialogRadioFlow = new System.Windows.Forms.FlowLayoutPanel();
+            this.dialogOkButton = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.ConsoleTab.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -73,7 +95,7 @@
             this.ConsoleTab.Location = new System.Drawing.Point(3, 3);
             this.ConsoleTab.Name = "ConsoleTab";
             this.ConsoleTab.SelectedIndex = 0;
-            this.ConsoleTab.Size = new System.Drawing.Size(1031, 574);
+            this.ConsoleTab.Size = new System.Drawing.Size(526, 574);
             this.ConsoleTab.TabIndex = 1;
             // 
             // tabPage1
@@ -82,7 +104,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1023, 548);
+            this.tabPage1.Size = new System.Drawing.Size(518, 548);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Console";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -93,7 +115,7 @@
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.richTextBox1.Location = new System.Drawing.Point(3, 3);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(1017, 542);
+            this.richTextBox1.Size = new System.Drawing.Size(512, 542);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.WhenKeyDown);
@@ -101,9 +123,11 @@
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.AutoSize = true;
-            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 505F));
             this.tableLayoutPanel1.Controls.Add(this.ConsoleTab, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -111,6 +135,178 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1037, 580);
             this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel3, 0, 2);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(535, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28.99408F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.00592F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 416F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(499, 574);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.playerYLabel);
+            this.panel1.Controls.Add(this.playerXLabel);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(493, 39);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player\'s Position";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Y";
+            // 
+            // playerXLabel
+            // 
+            this.playerXLabel.AutoSize = true;
+            this.playerXLabel.Location = new System.Drawing.Point(23, 19);
+            this.playerXLabel.Name = "playerXLabel";
+            this.playerXLabel.Size = new System.Drawing.Size(13, 13);
+            this.playerXLabel.TabIndex = 3;
+            this.playerXLabel.Text = "0";
+            // 
+            // playerYLabel
+            // 
+            this.playerYLabel.AutoSize = true;
+            this.playerYLabel.Location = new System.Drawing.Point(174, 19);
+            this.playerYLabel.Name = "playerYLabel";
+            this.playerYLabel.Size = new System.Drawing.Size(13, 13);
+            this.playerYLabel.TabIndex = 4;
+            this.playerYLabel.Text = "0";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.blip3);
+            this.panel2.Controls.Add(this.blip2);
+            this.panel2.Controls.Add(this.blip1);
+            this.panel2.Controls.Add(this.blip0);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(3, 48);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(493, 106);
+            this.panel2.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Random Blips";
+            // 
+            // blip0
+            // 
+            this.blip0.AutoSize = true;
+            this.blip0.BackColor = System.Drawing.Color.Red;
+            this.blip0.Location = new System.Drawing.Point(7, 17);
+            this.blip0.Name = "blip0";
+            this.blip0.Size = new System.Drawing.Size(13, 13);
+            this.blip0.TabIndex = 1;
+            this.blip0.Text = "0";
+            // 
+            // blip1
+            // 
+            this.blip1.AutoSize = true;
+            this.blip1.BackColor = System.Drawing.Color.Red;
+            this.blip1.Location = new System.Drawing.Point(26, 17);
+            this.blip1.Name = "blip1";
+            this.blip1.Size = new System.Drawing.Size(13, 13);
+            this.blip1.TabIndex = 2;
+            this.blip1.Text = "0";
+            // 
+            // blip2
+            // 
+            this.blip2.AutoSize = true;
+            this.blip2.BackColor = System.Drawing.Color.Red;
+            this.blip2.Location = new System.Drawing.Point(45, 17);
+            this.blip2.Name = "blip2";
+            this.blip2.Size = new System.Drawing.Size(13, 13);
+            this.blip2.TabIndex = 3;
+            this.blip2.Text = "0";
+            // 
+            // blip3
+            // 
+            this.blip3.AutoSize = true;
+            this.blip3.BackColor = System.Drawing.Color.Red;
+            this.blip3.Location = new System.Drawing.Point(64, 17);
+            this.blip3.Name = "blip3";
+            this.blip3.Size = new System.Drawing.Size(13, 13);
+            this.blip3.TabIndex = 4;
+            this.blip3.Text = "0";
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.dialogOkButton);
+            this.panel3.Controls.Add(this.dialogRadioFlow);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Location = new System.Drawing.Point(3, 160);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(493, 411);
+            this.panel3.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Dialog Subsystem";
+            // 
+            // dialogRadioFlow
+            // 
+            this.dialogRadioFlow.Location = new System.Drawing.Point(7, 21);
+            this.dialogRadioFlow.Name = "dialogRadioFlow";
+            this.dialogRadioFlow.Size = new System.Drawing.Size(200, 100);
+            this.dialogRadioFlow.TabIndex = 1;
+            // 
+            // dialogOkButton
+            // 
+            this.dialogOkButton.Location = new System.Drawing.Point(7, 128);
+            this.dialogOkButton.Name = "dialogOkButton";
+            this.dialogOkButton.Size = new System.Drawing.Size(75, 23);
+            this.dialogOkButton.TabIndex = 2;
+            this.dialogOkButton.Text = "OK";
+            this.dialogOkButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -127,6 +323,13 @@
             this.ConsoleTab.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,6 +344,24 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label playerYLabel;
+        private System.Windows.Forms.Label playerXLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label blip0;
+        private System.Windows.Forms.Label blip1;
+        private System.Windows.Forms.Label blip2;
+        private System.Windows.Forms.Label blip3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button dialogOkButton;
+        private System.Windows.Forms.FlowLayoutPanel dialogRadioFlow;
     }
 }
 
