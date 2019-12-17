7f5205652ee92ba3b5c6e0f56178a98d7eff8c0f
rocko <adam.preble@gmail.com>
2019-12-17 02:00:25 -0600

    Extracting future awaiter from original async-await demo

---
+++ CloacaInterpreter/FutureAwaiter.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace CloacaInterpreter
+{
+    /// Custom awaiter implemented like a future connected to the scheduler.
+    /// 
+    [Serializable]
+    public class FutureAwaiter<T> : System.Runtime.CompilerServices.INotifyCompletion, ISubscheduledContinuation
+    {
+        private T result;
+        private bool finished;
+        private Action continuation;
+        Interpreter interpreter;
+
+        public bool IsCompleted
+        {
+            get
+            {
+                return finished;
+            }
+        }
+
+        public string Text
+        {
+            get; protected set;
+        }
+
+        public FutureAwaiter(Interpreter interpreterToSubschedule)
+        {
+            finished = false;
+            interpreter = interpreterToSubschedule;
+        }
+
+        public void SetResult(T result)
+        {
+            this.result = result;
+            finished = true;
+            interpreter.Scheduler.NotifyUnblocked(this);
+        }
+
+        public void Continue()
+        {
+            continuation?.Invoke();
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            if (finished)
+            {
+                this.continuation();
+            }
+            else
+            {
+                this.continuation = continuation;
+            }
+        }
+
+        public FutureAwaiter<T> GetAwaiter()
+        {
+            return this;
+        }
+
+        public T GetResult()
+        {
+            return result;
+        }
+
+        public void AssignInterpreter(Interpreter interpreter)
+        {
+            this.interpreter = interpreter;
+        }
+    }
+
+
+    /// <summary>
+    /// Helper class to give the FutureAwaiter a void type. We can't do that by default since we can't
+    /// set void as a result type. We don't want to rely on null since we want a null result in the FutureAwaiter
+    /// to indicate actual null--or NoneType per the interpreter. Hence, we declare a custom object type. It is
+    /// implied that the interpreter will keep an eye out for this sentinel from FutureAwaiters and not push
+    /// anything on the stack if it sees it.
+    /// </summary>
+    [Serializable]
+    public class VoidSentinel
+    {
+        private VoidSentinel()
+        {
+
+        }
+
+        private static VoidSentinel __instance;
+
+        public static VoidSentinel Instance
+        {
+            get
+            {
+                if (__instance == null)
+                {
+                    __instance = new VoidSentinel();
+                }
+                return __instance;
+            }
+        }
+    }
+
+    // A void version because we can't use void as a parameter type.
+    [Serializable]
+    public class FutureVoidAwaiter : FutureAwaiter<VoidSentinel>
+    {
+        public FutureVoidAwaiter(Interpreter interpreterToSubschedule) : base(interpreterToSubschedule)
+        {
+        }
+
+        /// <summary>
+        /// Sets the result to the void sentinel. This is just a convenience helper so consumers don't need to
+        /// actually know about the void sentinel.
+        /// </summary>
+        public void SignalDone()
+        {
+            SetResult(VoidSentinel.Instance);
+        }
+    }
+}
