4dce8be3b577c9d66f7d1159e48f31a144c71971
rocko <adam.preble@gmail.com>
2020-02-02 12:09:13 -0600

    Switched type conversion lookup to use ValueTuples
    These are managed by value, not reference and don't have the same overhead as
    constructed objects. I also just keep a single cached ValueTuple instance that
    I use for lookups.
    
    I guess it's not thread-safe code, but the underlying dictionary isn't either,
    so meh.

--- LanguageImplementation/PyNetConverter.cs
+++ LanguageImplementation/PyNetConverter.cs
@@ -10,24 +10,28 @@ namespace LanguageImplementation
     {
         // This will lead to some problems if some asshat decides to subclass the base types. We won't be able to look it up this way.
         // At this point, I'm willing to dismiss that. Famous last words.
-        private static Dictionary<Tuple<Type, Type>, Func<object, object>> converters = new Dictionary<Tuple<Type, Type>, Func<object, object>>
+        private static Dictionary<ValueTuple<Type, Type>, Func<object, object>> converters = new Dictionary<ValueTuple<Type, Type>, Func<object, object>>
         {
-            { new Tuple<Type, Type>(typeof(int), typeof(PyInteger)), (as_int) => { return new PyInteger((int)as_int); } },
-            { new Tuple<Type, Type>(typeof(short), typeof(PyInteger)), (as_short) => { return new PyInteger((short)as_short); } },
-            { new Tuple<Type, Type>(typeof(long), typeof(PyInteger)), (as_long) => { return new PyInteger((long)as_long); } },
-            { new Tuple<Type, Type>(typeof(BigInteger), typeof(PyInteger)), (as_bi) => { return new PyInteger((BigInteger)as_bi); } },
-            { new Tuple<Type, Type>(typeof(PyInteger), typeof(int)), (as_pi) => { return (int) ((PyInteger)as_pi).number; } },
-            { new Tuple<Type, Type>(typeof(PyInteger), typeof(short)), (as_pi) => { return (short) ((PyInteger)as_pi).number; } },
-            { new Tuple<Type, Type>(typeof(PyInteger), typeof(long)), (as_pi) => { return (long) ((PyInteger)as_pi).number; } },
-            { new Tuple<Type, Type>(typeof(PyInteger), typeof(BigInteger)), (as_pi) => { return (BigInteger) ((PyInteger)as_pi).number; } },
+            { ValueTuple.Create(typeof(int), typeof(PyInteger)), (as_int) => { return new PyInteger((int)as_int); } },
+            { ValueTuple.Create(typeof(short), typeof(PyInteger)), (as_short) => { return new PyInteger((short)as_short); } },
+            { ValueTuple.Create(typeof(long), typeof(PyInteger)), (as_long) => { return new PyInteger((long)as_long); } },
+            { ValueTuple.Create(typeof(BigInteger), typeof(PyInteger)), (as_bi) => { return new PyInteger((BigInteger)as_bi); } },
+            { ValueTuple.Create(typeof(PyInteger), typeof(int)), (as_pi) => { return (int) ((PyInteger)as_pi).number; } },
+            { ValueTuple.Create(typeof(PyInteger), typeof(short)), (as_pi) => { return (short) ((PyInteger)as_pi).number; } },
+            { ValueTuple.Create(typeof(PyInteger), typeof(long)), (as_pi) => { return (long) ((PyInteger)as_pi).number; } },
+            { ValueTuple.Create(typeof(PyInteger), typeof(BigInteger)), (as_pi) => { return (BigInteger) ((PyInteger)as_pi).number; } },
         };
+
+        // We'll reuse a ValueTuple instead of constantly creating new ones.
+        private static ValueTuple<Type, Type> cachedKey = ValueTuple.Create<Type, Type>(null, null);
 
         public static object Convert(object fromObj, Type toType)
         {
-            var convert_rule = new Tuple<Type, Type>(fromObj.GetType(), toType);
-            if (converters.ContainsKey(convert_rule))
+            cachedKey.Item1 = fromObj.GetType();
+            cachedKey.Item2 = toType;
+            if (converters.ContainsKey(cachedKey))
             {
-                return converters[convert_rule].Invoke(fromObj);
+                return converters[cachedKey].Invoke(fromObj);
             }
             else
             {
@@ -37,8 +41,9 @@ namespace LanguageImplementation
 
         public static bool CanConvert(Type fromType, Type toType)
         {
-            var convert_rule = new Tuple<Type, Type>(fromType, toType);
-            return converters.ContainsKey(convert_rule);
+            cachedKey.Item1 = fromType;
+            cachedKey.Item2 = toType;
+            return converters.ContainsKey(cachedKey);
         }
     }
 }
