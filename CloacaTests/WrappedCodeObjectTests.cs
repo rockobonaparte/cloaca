78f1c803014c7ffb5fbe9728a78a6ad78be23890
rocko <adam.preble@gmail.com>
2020-02-17 00:59:47 -0600

    Reproduced situation of calling C# methods more conventionally
    The self argument does not get carried to the WrappedCodeObject. We need to
    figure out how to include it. It looks like it will have to get paired when
    doing LOAD_ATTR. The first way we do this will probably be too ham-fisted for
    final use, but it'll show if we have a handle on the problem.

---
+++ CloacaTests/WrappedCodeObjectTests.cs
@@ -0,0 +1,58 @@
+﻿using LanguageImplementation;
+using LanguageImplementation.DataTypes;
+
+using NUnit.Framework;
+
+
+namespace CloacaTests
+{
+
+    class TestPythonClass : PyClass
+    {
+        public TestPythonClass(CodeObject __init__) :
+            base("test_python_class", __init__, new PyClass[0])
+        {
+        }
+
+        private static TestPythonClass __instance;
+        public static TestPythonClass Instance
+        {
+            get
+            {
+                if (__instance == null)
+                {
+                    __instance = new TestPythonClass(null);
+                }
+                return __instance;
+            }
+        }
+        [ClassMember]
+        public static void hello_void(TestPythonObject self)
+        {
+            self.TestInt += 1;
+        }
+    }
+
+    class TestPythonObject : PyObject
+    { 
+        public int TestInt;
+
+        public TestPythonObject() : base(TestPythonClass.Instance)
+        {
+            TestInt = 0;
+        }
+    }
+
+    [TestFixture]
+    public class WrappedCodeObjectTests
+    {
+        [Test]
+        [Ignore("Just prototyped here for now to show the existence of the problem. We need to get the self pointer associated with the WrappedCodeObject")]
+        public void InvokingWrappedMethod()
+        {
+            var wrapper = new WrappedCodeObject("hello_void", typeof(TestPythonClass).GetMethod("hello_void"), this);
+            var instance = new TestPythonObject();
+            wrapper.Call(null, null, new object[0]);
+        }
+    }
+}
