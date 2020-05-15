d97a828bb54f530b1faefd5b3d3e17019c58168f
rocko <adam.preble@gmail.com>
2020-05-15 01:58:48 -0500

    Sketched failing test that shows mixed Python/.NET strings don't concatenate

--- CloacaTests/StringTests.cs
+++ CloacaTests/StringTests.cs
@@ -1,12 +1,8 @@
-﻿using System.Numerics;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 using NUnit.Framework;
 
 using LanguageImplementation.DataTypes;
-using LanguageImplementation.DataTypes.Exceptions;
-using LanguageImplementation;
-using System;
 
 namespace CloacaTests
 {
@@ -23,6 +19,23 @@ namespace CloacaTests
                 new VariableMultimap(new TupleList<string, object>
                 {
                     { "a", PyString.Create("Hello, World!") }
+                }), 1);
+        }
+
+        [Test]
+        [Ignore("Currently fails from mixing .NET with PyString. Winds up in DynamicDispatchOperation thinking it's working with numbers")]
+        public void StringConcatenateDotNetType()
+        {
+            // Making sure that we're properly parsing and generating all of these when there's multiples of the operator.
+            runBasicTest(
+                "a = a + ', World!'\n",
+                new Dictionary<string, object>
+                {
+                    { "a", "Hello" }
+                },
+                new VariableMultimap(new TupleList<string, object>
+                {
+                    { "a", "Hello, World!" }
                 }), 1);
         }
     }
