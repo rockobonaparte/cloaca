163feb121de86ff97d494bd9e785e7e152599b35
rocko <adam.preble@gmail.com>
2020-05-16 01:14:01 -0500

    Added test to show we don't return properly from conditions in functions

--- CloacaTests/ScopeTests.cs
+++ CloacaTests/ScopeTests.cs
@@ -40,6 +40,63 @@ namespace CloacaTests
                 }), 1, new string[] { "foo" });
         }
 
+        // Python 3:
+        //  3           0 LOAD_CONST               1 (2)
+        //              2 STORE_GLOBAL             0 (a)
+        //
+        //  4           4 LOAD_GLOBAL              0 (a)
+        //              6 LOAD_CONST               1 (2)
+        //              8 COMPARE_OP               2 (==)
+        //             10 POP_JUMP_IF_FALSE       16
+        //
+        //  5          12 LOAD_CONST               0 (None)
+        //             14 RETURN_VALUE
+        //
+        //  6     >>   16 LOAD_CONST               2 (3)
+        //             18 STORE_GLOBAL             0 (a)
+        //             20 LOAD_CONST               0 (None)
+        //             22 RETURN_VALUE
+        //
+        //
+        // Actual:
+        //  4           0  LOAD_CONST              0 (2)
+        //              3  STORE_GLOBAL            0 (a)
+        //
+        //  5           6  LOAD_GLOBAL             0 (a)
+        //              9  LOAD_CONST              1 (2)
+        //             12  COMPARE_OP              0 (0)
+        //
+        //  7          15  POP_JUMP_IF_FALSE        18
+        //             18  LOAD_CONST              2 (3)
+        //             21  STORE_GLOBAL            0 (a)
+        //             24  RETURN_VALUE  
+        //
+        /// <summary>
+        /// This one was added when I found out something wasn't working with returns inside conditionals. It actually
+        /// looks like the code wasn't even getting generated correctly.
+        /// </summary>
+        [Test]
+        [Ignore("Exposed this during Unity testing. A fix is necessary. It looks to be a problem in code generation!")]
+        public void ConditionalReturn()
+        {
+            string program =
+                "a = 1\n" +
+                "def foo():\n" +
+                "   global a\n" +
+                "   a = 2\n" +
+                "   if a == 2:\n" +
+                "      return\n" +
+                "   a = 3\n" +
+                "\n" +
+                "foo()\n";
+
+            runBasicTest(program,
+                new VariableMultimap(new TupleList<string, object>
+                {
+                    { "a", PyInteger.Create(2) }
+                }), 1, new string[] { "foo" });
+        }
+
         [Test]
         public void ImplicitlyUsesGlobal()
         {
