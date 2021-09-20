148b55a6cfdb503cbd04728dd61f6709ef556aa2
rocko <adam.preble@gmail.com>
2021-09-20 01:57:09 -0500

    Gross first draft of a string module loader
    It just takes a dictionary of module names and maps code strings to them.
    
    We'll use this to enhance unit testing.

---
+++ CloacaInterpreter/ModuleImporting/StringCodeModuleLoader.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using LanguageImplementation.DataTypes;
+using LanguageImplementation;
+using System.Threading.Tasks;
+using System;
+
+namespace CloacaInterpreter.ModuleImporting
+{
+    /// <summary>
+    /// A module spec loader for code represented as raw strings. This is mostly used for testing, but could plausibly
+    /// be used to construct a hierarchy of uncompiled code in general use. There, it would probably be better to at least
+    /// use precompiled code (or cache the CodeObjects afterwards)
+    /// 
+    /// It does not run __init__.py files when loading modules yet. See [MODULE_INIT] in TODO. It might never do that
+    /// given the focus on this finder on specific testing.
+    /// </summary>
+    public class StringCodeModuleFinder : ISpecFinder
+    {
+        public Dictionary<string, string> CodeLookup;      // 'some.module.path' = print("Pile of code")
+        private StringCodeModuleLoader loader;
+
+        public StringCodeModuleFinder()
+        {
+            this.CodeLookup = new Dictionary<string, string>();
+            this.loader = new StringCodeModuleLoader();
+        }
+
+        public PyModuleSpec find_spec(FrameContext context, string name, string import_path, PyModule target)
+        {
+            if(CodeLookup.ContainsKey(name))
+            {
+                var spec = PyModuleSpec.Create(name, loader, "", null);
+                spec.LoaderState = name;
+                return spec;
+            }
+
+            // Fall-through: Module not found. Return null according to specifications.
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Loader of FileBasedModuleFinder PyModuleSpecs. This will actually load the module
+    /// from disk. It will then execute the code inside that module file. The final module is a fresh
+    /// module created from scratch that is populated with the executed code's namespace.
+    /// </summary>
+    public class StringCodeModuleLoader : ISpecLoader
+    {
+        private Dictionary<string, string> lookup;
+
+        public StringCodeModuleLoader(Dictionary<string, string> lookup)
+        {
+            this.lookup = lookup;
+        }
+
+        public async Task<object> Load(IInterpreter interpreter, FrameContext context, PyModuleSpec spec)
+        {
+            var code = lookup[(string)spec.LoaderState];
+            var moduleCode = await ByteCodeCompiler.Compile(inFile, new Dictionary<string, object>(), interpreter.Scheduler);
+
+            string modulename = spec.Origin.Length == 0 ? spec.Name : spec.Origin + "." + spec.Name;
+            await interpreter.CallInto(context, moduleCode, new object[0], modulename);
+
+            if(context.EscapedDotNetException != null)
+            {
+                throw context.EscapedDotNetException;
+            }
+
+            var moduleFrame = context.callStack.Pop();
+            var module = PyModule.Create(spec.Name);
+            
+            for(int local_i = 0; local_i < moduleFrame.LocalNames.Count; ++local_i)
+            {
+                var name = moduleFrame.LocalNames[local_i];
+                if (moduleFrame.Locals.ContainsKey(name))
+                {
+                    module.__setattr__(name, moduleFrame.Locals[name]);
+                }
+            }
+
+            return module;
+        }
+    }
+}
