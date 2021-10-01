ff17ce989b1541f278d26f71021d7f0fc205db0d
rocko <adam.preble@gmail.com>
2021-10-01 01:13:46 -0500

    Start of a PyFunction

---
+++ LanguageImplementation/DataTypes/PyFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LanguageImplementation.DataTypes
+{
+    public class PyFunction : PyObject, IPyCallable
+    {
+        // needs a reference to:
+        // 1. CodeObject
+        // 2. Globals           (and how does it get this? When would it be created?)
+
+        private IPyCallable callable;
+
+        public PyFunction(IPyCallable callable)
+        {
+            this.callable = callable;
+            __setattr__("__call__", this);
+        }
+
+        public Task<object> Call(IInterpreter interpreter, FrameContext context, object[] args)
+        {
+            return callable.Call(interpreter, context, args);
+        }
+    }
+}
