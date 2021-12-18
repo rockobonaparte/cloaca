ec80fef7baf4d08e8c6c48277a48acd791397583
rocko <adam.preble@gmail.com>
2021-12-18 01:37:53 -0600

    Start to draft an iterative merge sort
    I think I have to write our own sorting algorithm so we I can await on the
    comparisons. This just plays with ints for starter so I can make sure the
    algorithm is superficially okay. I don't tend to write iterative merge sorts!

---
+++ CloacaInterpreter/Sorting.cs
@@ -0,0 +1,32 @@
+﻿namespace CloacaInterpreter
+{
+    public class Sorting
+    {
+        private static void Swap(int[] arr, int a, int b)
+        {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+        public static void Sort(int[] arr)
+        {
+            // Iterative merge sort
+            for (int width = 1; width < arr.Length; width <<= 1)
+            {
+                for(int l_start = 0; l_start < arr.Length; l_start += 2 * width)
+                {
+                    int r_start = l_start + width;
+                    int l = l_start, r = r_start;
+                    for(int i = l_start; i < r_start && r < r_start + width; ++i)
+                    {
+                        if(arr[i] > arr[r])
+                        {
+                            Swap(arr, i, r);
+                            r += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CloacaTests/SortingTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using CloacaInterpreter;
+
+namespace CloacaTests
+{
+    [TestFixture]
+    public class SortingTests
+    {
+        [Test]
+        public void BasicSorting()
+        {
+            int[] test = { 4, 3, 2, 1 };
+            Sorting.Sort(test);
+            Assert.That(test, Is.EqualTo(new int[] { 1, 2, 3, 4 }));
+        }
+    }
+}
