ea43df5b230b9a988d1571a75f89a85389c3a83d
rocko <adam.preble@gmail.com>
2021-12-19 12:03:39 -0600

    Hardened sort against various sizes

--- CloacaInterpreter/Sorting.cs
+++ CloacaInterpreter/Sorting.cs
@@ -15,14 +15,32 @@
             {
                 for(int l_start = 0; l_start < arr.Length; l_start += 2 * width)
                 {
+
                     int r_start = l_start + width;
-                    int l = l_start, r = r_start;
-                    for(int i = l_start; i < r_start && r < r_start + width; ++i)
+
+                    // The right-side width (r_width) should not exceed the array length.
+                    // We will use it later to keep r from exiting the right-side merge window.
+                    int r_width = width;
+                    if(r_start + r_width > arr.Length)
+                    {
+                        r_width -= r_start + r_width - arr.Length;
+                    }
+
+                    r_start = r_start >= arr.Length ? arr.Length - 1 : r_start;
+
+                    int r = r_start;
+                    for(int i = l_start; i < r_start && r < arr.Length; ++i)
                     {
                         if(arr[i] > arr[r])
                         {
                             Swap(arr, i, r);
-                            r += 1;
+                            if(r < r_start + r_width - 1)
+                            {
+                                // Keep r in-place as the last index for swapping
+                                // if we have more left indices to work on, but move up
+                                // otherwise.
+                                r += 1;
+                            }
                         }
                     }
                 }
--- CloacaTests/SortingTests.cs
+++ CloacaTests/SortingTests.cs
@@ -13,9 +13,24 @@ namespace CloacaTests
         [Test]
         public void BasicSorting()
         {
-            int[] test = { 4, 3, 2, 1 };
-            Sorting.Sort(test);
-            Assert.That(test, Is.EqualTo(new int[] { 1, 2, 3, 4 }));
+            int[] test0 = { };
+            int[] test1 = { 1 };
+            int[] test2 = { 2, 1 };
+            int[] test3 = { 3, 2, 1 };
+            int[] test4 = { 4, 3, 2, 1 };
+            int[] test5 = { 5, 4, 3, 2, 1 };
+            Sorting.Sort(test0);
+            Sorting.Sort(test1);
+            Sorting.Sort(test2);
+            Sorting.Sort(test3);
+            Sorting.Sort(test4);
+            Sorting.Sort(test5);
+            Assert.That(test0, Is.EqualTo(new int[] { }));
+            Assert.That(test1, Is.EqualTo(new int[] { 1 }));
+            Assert.That(test2, Is.EqualTo(new int[] { 1, 2 }));
+            Assert.That(test3, Is.EqualTo(new int[] { 1, 2, 3 }));
+            Assert.That(test4, Is.EqualTo(new int[] { 1, 2, 3, 4 }));
+            Assert.That(test5, Is.EqualTo(new int[] { 1, 2, 3, 4, 5 }));
         }
     }
 }
