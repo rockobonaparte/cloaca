a445109885477154924bd26c6de866ad187cb730
rocko <adam.preble@gmail.com>
2022-03-26 01:51:11 -0500

    Unintegrated draft of a variable scope visitor
    I learned that CPython uses a 2-part parsing process where the first part determines
    the scope of named items. This is how it determines nonlocal stuff in particular.
    This visitor isn't actually used in the main code yet, but has been tested against
    some scenarios and should be ready to try.
    
    Integrating this will be essential for properly handling enclosed scope and emitting
    DEREF instructions.
    
    I'm not 100% convinced it's completely correct yet, but I expect to find out details
    when I start integrating.

---
+++ CloacaInterpreter/Visitors/VariableScanVisitor.cs
@@ -0,0 +1,366 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Antlr4.Runtime.Misc;
+
+using Language;
+
+public enum NameScope
+{
+    Undefined,
+    Local,
+    EnclosedRead,
+    EnclosedReadWrite,
+    Global,
+}
+
+/// <summary>
+/// Data structure for managing node names across layers of code. This is used to tell in the code generation pass
+/// whether or not to treat a variable as global, enclosed, or local.
+/// </summary>
+public class CodeNamesNode
+{
+    public CodeNamesNode Parent;
+    public Dictionary<string, NameScope> NamedScopes;
+    public Dictionary<string, CodeNamesNode> Children;
+
+    // GlobalsSet: Globals that really came from the outside. Think functions and reserved named stuff.
+    public HashSet<string> GlobalsSet;
+
+    public CodeNamesNode()
+    {
+        GlobalsSet = new HashSet<string>();
+        NamedScopes = new Dictionary<string, NameScope>();
+        Children = new Dictionary<string, CodeNamesNode>();
+    }
+
+    // Alternate version that takes a list of variables from the outside to treat like globals.
+    public CodeNamesNode(IEnumerable<string> externalGlobals) : this()
+    {
+        foreach(var global in externalGlobals)
+        {
+            GlobalsSet.Add(global);
+        }
+    }
+
+    public CodeNamesNode(HashSet<string> globalsSet)
+    {
+        GlobalsSet = globalsSet;
+        NamedScopes = new Dictionary<string, NameScope>();
+        Children = new Dictionary<string, CodeNamesNode>();
+    }
+
+    // TODO: I think this actually has to keep going up and up and up.
+    private void updateScope(string name, NameScope newScope)
+    {
+        if(NamedScopes.ContainsKey(name))
+        {
+            NamedScopes[name] = newScope;
+        }
+
+        foreach(var child in Children.Values)
+        { 
+            child.updateScope(name, newScope);
+        }
+    }
+
+    public void AddName(string name)
+    {
+        AddName(name, NameScope.Local);
+    }
+
+    private NameScope selectBroadestScope(NameScope a, NameScope b)
+    {
+        if(a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+
+    public void AddName(string name, NameScope scope)
+    {
+        // I think this can be optimized to stop when a local scope becomes enclosing or global, but I
+        // need to see how things proceed with them before I go all-in on the optimization. I'm running on
+        // a notion that we don't have tons and tons and tons of functions inside each other but who knows.
+        var selectedScope = scope;
+
+        // Root is global and overrides whatever we think it currently is.
+        if(Parent == null)
+        {
+            selectedScope = NameScope.Global;
+        }
+
+        if(GlobalsSet.Contains(name))
+        {
+            selectedScope = NameScope.Global;
+        } 
+
+        if (!NamedScopes.ContainsKey(name))
+        {
+            NamedScopes.Add(name, selectedScope);
+        }
+        else
+        {
+            selectedScope = selectBroadestScope(scope, NamedScopes[name]);
+            NamedScopes[name] = selectedScope;
+        }
+
+        CodeNamesNode lastFoundAbove = this;
+        NameScope aboveScope = NameScope.Undefined;
+        for (CodeNamesNode itr = Parent; itr != null; itr = itr.Parent)
+        {
+            if (itr.NamedScopes.ContainsKey(name))
+            {
+                lastFoundAbove = itr;
+                aboveScope = itr.NamedScopes[name];
+            }
+        }
+
+        if (lastFoundAbove != this)
+        {
+            if(selectedScope == NameScope.Local && aboveScope != NameScope.Global)
+            {
+                selectedScope = NameScope.EnclosedRead;
+            }
+
+            // Global doesn't propagate "up" so much as it propagates globally.
+            // A higher level usage of the same name will just be a local unless
+            // either at root scope or marked as global with the global keyword
+            // explicitly.
+            if(selectedScope != NameScope.Global && aboveScope != NameScope.Global)
+            {
+                lastFoundAbove.updateScope(name, selectedScope);
+            }
+        }
+    }
+
+    public string ToReportString(int indent=0)
+    {
+        var b = new StringBuilder();
+        var keys = NamedScopes.Keys.ToList();
+        keys.Sort();
+
+        // We're not using AppendLine because it produces a classic Windows
+        // \r\n and I don't want to have to put both in my assertions because
+        // it looks like garbage.
+        foreach(var key in keys)
+        {
+            b.Append(new string(' ', indent));
+            b.Append(key);
+            b.Append(": ");
+            b.Append(NamedScopes[key].ToString());
+            b.Append("\n");
+        }
+
+        keys = Children.Keys.ToList();
+        keys.Sort();
+        foreach(var key in keys)
+        {
+            b.Append(new string(' ', indent));
+            b.Append(key);
+            b.Append(":\n");
+            b.Append(Children[key].ToReportString(indent + 2));
+        }
+
+        return b.ToString();
+    }
+}
+
+public class VariableScanVisitor : CloacaBaseVisitor<object>
+{
+    private CodeNamesNode rootNode;
+    private CodeNamesNode currentNode;
+
+    public CodeNamesNode RootNode
+    {
+        get
+        {
+            return rootNode;
+        }
+    }
+
+    private CodeNamesNode descendFromName(string new_name)
+    {
+        var newNode = new CodeNamesNode(rootNode.GlobalsSet);
+        currentNode.Children.Add(new_name, newNode);
+        newNode.Parent = currentNode;
+        currentNode = newNode;
+        return currentNode;
+    }
+
+    private CodeNamesNode ascendNameNode()
+    {
+        currentNode = currentNode.Parent;
+        return currentNode;
+    }
+
+    public VariableScanVisitor(IEnumerable<string> names)
+    {
+        rootNode = new CodeNamesNode(names);
+        currentNode = rootNode;
+    }
+
+    public override object VisitExpr_stmt([NotNull] CloacaParser.Expr_stmtContext context)
+    {
+        if (context.testlist_star_expr().Length == 1)
+        {
+            if(context.testlist() != null)
+            {
+                Visit(context.testlist());
+            }
+            Visit(context.testlist_star_expr(0));
+        }
+        else
+        {
+            Visit(context.testlist_star_expr(context.testlist_star_expr().Length - 1));
+            for (int lvalue_i = context.testlist_star_expr().Length - 2; lvalue_i >= 0; --lvalue_i)
+            {
+                for (int test_i = 0; test_i < context.testlist_star_expr(lvalue_i).test().Length; ++test_i)
+                {
+                    VisitLValueTestlist_star_expr(context.testlist_star_expr(lvalue_i).test()[test_i]);
+                }
+            }
+        }
+        return null;
+    }
+
+    public object VisitLValueTestlist_star_expr([NotNull] CloacaParser.TestContext context)
+    {
+        var expr = context.or_test()[0].and_test()[0].not_test()[0].comparison().expr()[0];
+        var variableName = expr.GetText();
+        currentNode.AddName(variableName);
+        return null;
+    }
+
+    public override object VisitAtom_expr([NotNull] CloacaParser.Atom_exprContext context)
+    {
+        var trailers = context.trailer();
+        if(trailers.Length > 0)
+        {
+            Visit(context.atom());
+            for (int trailer_i = 0; trailer_i < context.trailer().Length; ++trailer_i)
+            {
+                var trailer = context.trailer(trailer_i);
+                if (trailer.NAME() != null)
+                {
+                    var attrName = trailer.NAME().GetText();
+                    currentNode.AddName(attrName);
+
+                }
+
+                else if (trailer.arglist() != null || trailer.GetText() == "()")
+                {
+                    int argIdx = 0;
+                    for (argIdx = 0; trailer.arglist() != null &&
+                        trailer.arglist().argument(argIdx) != null; ++argIdx)
+                    {
+                        if (trailer.arglist().argument(argIdx).test().Length > 1)
+                        {
+                            base.Visit(trailer.arglist().argument(argIdx).test(1));
+                        }
+                        else
+                        {
+                            base.Visit(trailer.arglist().argument(argIdx));
+                        }
+                    }
+                }
+                else
+                {
+                    base.Visit(trailer);
+                }
+            }
+            return null;
+        }
+        else
+        {
+            return base.VisitAtom_expr(context);
+        }
+    }
+
+    public override object VisitAtomSquareBrackets([NotNull] CloacaParser.AtomSquareBracketsContext context)
+    {
+        return base.VisitAtomSquareBrackets(context);
+    }
+
+    public override object VisitTrailer([NotNull] CloacaParser.TrailerContext context)
+    {
+        return base.VisitTrailer(context);
+    }
+
+    public override object VisitAtomName([NotNull] CloacaParser.AtomNameContext context)
+    {
+        var variableName = context.GetText();
+        currentNode.AddName(variableName);
+        return null;
+    }
+
+    public override object VisitTfpdef([NotNull] CloacaParser.TfpdefContext context)
+    {
+        var variableName = context.GetText();
+        currentNode.AddName(variableName);
+        return null;
+    }
+
+    public override object VisitFuncdef([NotNull] CloacaParser.FuncdefContext context)
+    {
+        descendFromName(context.NAME().GetText());
+        base.Visit(context.parameters());
+        base.VisitSuite(context.suite());
+        ascendNameNode();
+        return null;
+    }
+
+    public override object VisitNonlocal_stmt([NotNull] CloacaParser.Nonlocal_stmtContext context)
+    {
+        foreach(var variableName in context.NAME())
+        {
+            currentNode.AddName(variableName.GetText(), NameScope.EnclosedReadWrite);
+        }
+        return null;
+    }
+
+    public override object VisitGlobal_stmt([NotNull] CloacaParser.Global_stmtContext context)
+    {
+        foreach (var variableName in context.NAME())
+        {
+            currentNode.AddName(variableName.GetText(), NameScope.Global);
+        }
+        return null;
+    }
+
+    public override object VisitExcept_clause([NotNull] CloacaParser.Except_clauseContext context)
+    {
+        base.VisitExcept_clause(context);
+        if(context.NAME() != null)
+        {
+            currentNode.AddName(context.NAME().GetText());
+        }
+        return null;
+    }
+
+    public override object VisitDotted_as_name([NotNull] CloacaParser.Dotted_as_nameContext context)
+    {
+        base.VisitDotted_as_name(context);
+        if (context.NAME() != null)
+        {
+            currentNode.AddName(context.NAME().GetText());
+        }
+        return null;
+    }
+
+    public override object VisitImport_as_name([NotNull] CloacaParser.Import_as_nameContext context)
+    {
+        base.VisitImport_as_name(context);
+        if (context.NAME() != null && context.NAME().Length > 0)
+        {
+            currentNode.AddName(context.NAME()[0].GetText());
+        }
+        return null;
+    }
+
+}
---
+++ CloacaTests/VariableScanVisitorTests.cs
@@ -0,0 +1,322 @@
+﻿using Antlr4.Runtime;
+using NUnit.Framework;
+
+using Language;
+using LanguageImplementation;
+using System.Collections.Generic;
+using System;
+
+namespace CloacaTests
+{
+    [TestFixture]
+    class VariableScanVisitorTests
+    {
+        public void RunTest(string program, string compare_dump, string[] globals = null)
+        {
+            var inputStream = new AntlrInputStream(program);
+            var lexer = new CloacaLexer(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+            var errorListener = new ParseErrorListener();
+            var parser = new CloacaParser(commonTokenStream);
+            parser.AddErrorListener(errorListener);
+
+            var antlrVisitorContext = parser.file_input();
+
+            errorListener.AssertNoErrors();
+
+            var visitor = new VariableScanVisitor(globals ?? new string[0]);
+            visitor.Visit(antlrVisitorContext);
+
+            var rootNamesKeys = visitor.RootNode.NamedScopes.Keys;
+
+            var result = visitor.RootNode.ToReportString();
+
+            Assert.That(result, Is.EqualTo(compare_dump));
+        }
+
+        [Test]
+        public void Hello()
+        {
+            // Root is global!!!
+            string program = "a = 1\n";
+            RunTest(program, "a: Global\n");
+        }
+
+        [Test]
+        public void HelloFunc()
+        {
+            string program = "a = 1\n" +
+                             "def foo():\n" +
+                             "   b = 2\n" +
+                             "   return b";
+            RunTest(program, "a: Global\n" +
+                             "foo:\n" +
+                             "  b: Local\n");
+        }
+
+        [Test]
+        public void FuncParameters()
+        {
+            string program = "a = 1\n" +
+                             "def foo(c):\n" +
+                             "   b = 2\n" +
+                             "   return b";
+            RunTest(program, "a: Global\n" +
+                             "foo:\n" +
+                             "  b: Local\n" +
+                             "  c: Local\n");
+        }
+
+        [Test]
+        public void BasicGlobalPlumbing()
+        {
+            string program = "a = 1\n";
+            RunTest(program, "a: Global\n", new string[] { "a" });
+        }
+
+        [Test]
+        public void GlobalDeclaration()
+        {
+            string program = "global a\n";
+            RunTest(program, "a: Global\n");
+        }
+
+        [Test]
+        public void CallFuncOneArg()
+        {
+            string program =
+                "print(a)\n";
+
+            RunTest(program,
+                             "a: Global\n" +
+                             "print: Global\n", new string[] { "a", "print" });
+        }
+
+        [Test]
+        public void InnerGlobalOuterLocal()
+        {
+            string program =
+                "def outer():\n" +      // Defined but never invoked so doesn't come up as a global.
+                "  a = 100\n" +
+                "  def inner():\n" +
+                "    global a\n" +
+                "    a += 1\n" +        // PS: Actually run this will get you an error that a isn't defined.
+                "    return a\n" +
+                "  return a + inner()\n";
+
+            
+            RunTest(program,
+                             "outer:\n" +
+                             "  a: Local\n" +
+                             "  inner: Local\n" +
+                             "  inner:\n" +
+                             "    a: Global\n");
+        }
+
+        [Test]
+        public void GlobalInFunction()
+        {
+            string program = "def fun():\n" +
+                             "  a = 1\n";
+            RunTest(program, "fun:\n" +
+                             "  a: Global\n", new string[] { "a" });
+        }
+
+        /// <summary>
+        /// This came from ScopeTests and was one of two that motivated adding a first-pass to get
+        /// variable context.
+        /// </summary>        
+        [Test]
+        public void InnerFunctionReadsOuter()
+        {
+            string program =
+                "def outer():\n" +
+                "  a = 100\n" +
+                "  def inner():\n" +
+                "    return a + 1\n" +
+                "  b = inner()\n" +
+                "  return b\n" +
+                "c = outer()\n";
+
+            RunTest(program, "c: Global\n" +
+                             "outer: Global\n" +
+                             "outer:\n" +
+                             "  a: EnclosedRead\n" +
+                             "  b: Local\n" +
+                             "  inner: Local\n" +
+                             "  inner:\n" +
+                             "    a: EnclosedRead\n");
+        }
+
+        /// <summary>
+        /// This came from ScopeTests and was test two of two that motivated adding a first-pass to get
+        /// variable context.
+        /// 
+        /// It also adds the nonlocal to make variable 'a' writeable.
+        /// </summary>
+        [Test]
+        public void InnerFunctionWritersOuterNonlocal()
+        {
+            string program =
+                "def outer():\n" +
+                "  a = 100\n" +
+                "  def inner():\n" +
+                "    nonlocal a\n" +
+                "    a += 1\n" +
+                "    return a\n" +
+                "  return a + inner()\n" +
+                "b = outer()\n";
+
+            // TODO: I think I need additional context to tell if something is enclosed and writable!
+            RunTest(program, "b: Global\n" +
+                             "outer: Global\n" +
+                             "outer:\n" +
+                             "  a: EnclosedReadWrite\n" +
+                             "  inner: Local\n" +
+                             "  inner:\n" +
+                             "    a: EnclosedReadWrite\n");
+        }
+
+        [Test]
+        public void MultilevelNonlocal()
+        {
+            string program =
+                            "def f1():\n" +
+                            "  a = 2\n" +
+                            "  def f2():\n" +
+                            "    def f3():\n" +
+                            "      nonlocal a\n" +
+                            "      a = 3\n" +
+                            "    f3()\n" +
+                            "  f2()\n" +
+                            "  return a\n";
+
+            // TODO: I think I need additional context to tell if something is enclosed and writable!
+            RunTest(program, "f1:\n" +
+                             "  a: EnclosedReadWrite\n" +
+                             "  f2: Local\n" +
+                             "  f2:\n" +
+                             "    f3: Local\n" +
+                             "    f3:\n" +
+                             "      a: EnclosedReadWrite\n");
+        }
+
+        [Test]
+        public void MultilevelGlobal()
+        {
+            string program =
+                            "a = 2\n" +
+                            "def f1():\n" +
+                            "  def f2():\n" +
+                            "    def f3():\n" +
+                            "      global a\n" +
+                            "      a = 3\n" +
+                            "    f3()\n" +
+                            "  f2()\n";
+
+            // TODO: I think I need additional context to tell if something is enclosed and writable!
+            RunTest(program, "a: Global\n" +
+                             "f1:\n" +
+                             "  f2: Local\n" +
+                             "  f2:\n" +
+                             "    f3: Local\n" +
+                             "    f3:\n" +
+                             "      a: Global\n");
+        }
+
+        [Test]
+        public void ParsingBundledStuff()
+        {
+            string program = "arr = [1, b]\n" +
+                             "hash = {'foo': c}\n" +
+                             "tup = {2, d}\n" +
+                             "set = (3, e)\n" +
+                             "func = call(4, f)\n";
+
+            RunTest(program, "arr: Global\n" +
+                             "b: Global\n" +
+                             "c: Global\n" +
+                             "call: Global\n" +
+                             "d: Global\n" +
+                             "e: Global\n" +
+                             "f: Global\n" +
+                             "func: Global\n" +
+                             "hash: Global\n" +
+                             "set: Global\n" +
+                             "tup: Global\n",
+                             new string[] { "b", "c", "d", "e", "f", "call" }
+                             );
+        }
+
+        // TODO: Add a test that tries various grammars to make sure we get variables in things like:
+        // for-loops
+        // try-except blocks
+        // conditionals
+        // Arrays
+        // Functions inside functions
+        // ...and more...
+        [Test]
+        public void ParseVariousBlocks()
+        {
+            string program =
+                "for for_i in range(10):\n" +
+                "  in_for = for_i + 1\n" +
+                "try:\n" +
+                "  try_var = 3\n" +
+                "except Exception as exc_var:" +
+                "  exc_blk_var = exc_var\n";
+
+            RunTest(program, "exc_blk_var: Global\n" +
+                             "exc_var: Global\n" +
+                             "Exception: Global\n" +
+                             "for_i: Global\n" +
+                             "in_for: Global\n" +
+                             "range: Global\n" +
+                             "try_var: Global\n",
+                             new string[] { "Exception", "range" }
+                             );
+        }
+
+        [Test]
+        public void MashedUpPileOfLocalGlobalNonlocal()
+        {
+            string program =
+                "a = -1\n" +
+                "def f1():\n" +
+                "  a = 2\n" +
+                "  def f2():\n" +
+                "    def f3():\n" +
+                "      global a\n" +
+                "      a = 10000\n" +
+                "      def f4():\n" +
+                "        a = 5\n" +
+                "        return a\n" +
+                "      a += f4()\n" +
+                "      return a\n" +
+                "    \n" +
+                "    nonlocal a\n" +
+                "    a -= f3()\n" +
+                "    return a\n" +
+                "  a += f2()\n" +
+                "  return a\n" +
+                "a += f1()\n" +
+                "print(a)\n";
+
+            RunTest(program, "a: Global\n" +
+                             "f1: Global\n" +
+                             "print: Global\n" +
+                             "f1:\n" +
+                             "  a: Local\n" +
+                             "  f2: Local\n" +
+                             "  f2:\n" +
+                             "    a: EnclosedReadWrite\n" +
+                             "    f3: Local\n" +
+                             "    f3:\n" +
+                             "      a: Global\n" +
+                             "      f4: Local\n" +
+                             "      f4:\n" +
+                             "        a: Local\n", new string[] { "print" });
+        }
+
+    }
+}
