88c2d0421eb13a0014e8ea9626656e254d082db7
rocko <adam.preble@gmail.com>
2022-04-03 16:36:30 -0500

    Writing tests to replicate problem I'm initially solving

--- CloacaTests/VariableScanVisitorTests.cs
+++ CloacaTests/VariableScanVisitorTests.cs
@@ -65,5 +65,59 @@ namespace CloacaTests
                              "  b: Local\n" +
                              "  c: Local\n");
         }
+
+        /// <summary>
+        /// This came from ScopeTests and was one of two that motivated adding a first-pass to get
+        /// variable context.
+        /// </summary>        
+        [Test]
+        [Ignore("Enclosed scope logic not yet implemented")]
+        public void InnerFunctionReadsOuter()
+        {
+            string program =
+                "def outer():\n" +
+                "  a = 100\n" +
+                "  def inner():\n" +
+                "    return a + 1\n" +
+                "  b = inner()\n" +
+                "  return b\n" +
+                "c = outer()\n";
+
+            RunTest(program, "c: Local:\n" +
+                             "outer:\n" +
+                             "  a: Enclosed\n" +
+                             "  b: Local\n" +
+                             "  inner:\n" +
+                             "    a: Enclosed\n");
+        }
+
+        /// <summary>
+        /// This came from ScopeTests and was test two of two that motivated adding a first-pass to get
+        /// variable context.
+        /// 
+        /// It also adds the nonlocal to make variable 'a' writeable.
+        /// </summary>
+        [Test]
+        [Ignore("nonlocal not yet implemented")]
+        public void InnerFunctionWritersOuterNonlocal()
+        {
+            string program =
+                "def outer():\n" +
+                "  a = 100\n" +
+                "  def inner():\n" +
+                "    nonlocal a\n" +
+                "    a += 1\n" +
+                "    return a\n" +
+                "  return a + inner()\n" +
+                "b = outer()\n";
+
+            // TODO: I think I need additional context to tell if something is enclosed and writable!
+            RunTest(program, "c: Local:\n" +
+                             "outer:\n" +
+                             "  a: Enclosed\n" +
+                             "  b: Local\n" +
+                             "  inner:\n" +
+                             "    a: Enclosed\n");
+        }
     }
 }
